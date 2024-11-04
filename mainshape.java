06e03ac7a4f93c18ff7585eefe23811d7164ce8e
Akshitwadhwa <akshitwadhwa06@gmail.com>
2024-11-04 13:56:57 +0530

    q3

--- mainshape.java
+++ mainshape.java
@@ -1,14 +1,13 @@
-public class shape {
+ class shape {
 
     // since we dont know the area yet , we have to define it as a double area
     public double getarea(){
         System.out.println("area of the shape is still unknown");
-    }
+    
 }
 
 // creating a new class rectangle that extends shape
 // we will also define the constructor
-
 
 class rectangle extends shape{
     private double lenght;
@@ -17,10 +16,9 @@ class rectangle extends shape{
 
     public rectangle(double lenght, double width) {
         this.lenght = lenght;
-        this.lenght = lenght;
+        this.width = width;
 
     }
-
     @Override
     public double getarea(){
         return lenght * width;
@@ -28,19 +26,21 @@ class rectangle extends shape{
     }
     }
 
- class main{
-        public static void main(String[] args) {
+  public class  mainshape {
+  
+            public static void main(String[] args) {
             shape sh = new shape();
             System.out.println("area of the shape:" +sh.getarea());
 
             // for the last part we need to add a new rectangle object
             
-            rectangle rect = new rectangle();
+            rectangle1 rect = new rectangle1();
             System.out.println("Area of rectangle:" + rect.getarea());
             
     
         }
 
+ }
  }
 
     
